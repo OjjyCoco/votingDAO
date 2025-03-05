20d65032eee0e12ea3fea153f4475e8473f73e95
Nathan <nathanheckmann@gmail.com>
2025-03-05 20:46:53 +0100

    Components change

--- frontend/voting/components/shared/ProposalsRegistrationStarted.jsx
+++ frontend/voting/components/shared/ProposalsRegistrationStarted.jsx
@@ -1,4 +1,12 @@
-export default function ProposalsRegistrationStarted() {
-    return <div>Proposal registration has started! Submit your proposals.</div>;
+// export default function ProposalsRegistrationStarted() {
+//     return <div>Proposal registration has started! Submit your proposals.</div>;
+//   }
+
+  
+  const ProposalsRegistrationStarted = () => {
+    return (
+      <div>Proposal registration has started! Submit your proposals</div>
+    )
   }
   
+  export default ProposalsRegistrationStarted
--- frontend/voting/components/shared/RegisteringVoters.jsx
+++ frontend/voting/components/shared/RegisteringVoters.jsx
@@ -1,4 +1,13 @@
-export default function RegisteringVoters() {
-    return <div>Voter registration is currently open.</div>;
-  }
+// export default function RegisteringVoters() {
+//     return <div>Voter registration is currently open.</div>;
+//   }
   
+// import React from 'react'
+
+const RegisteringVoters = () => {
+  return (
+    <div>Voter registration is currently open.</div>
+  )
+}
+
+export default RegisteringVoters

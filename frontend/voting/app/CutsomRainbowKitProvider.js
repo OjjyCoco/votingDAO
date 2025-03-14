0f2caf5f2c98b4a6f3b9ca25dd72c96a736404d3
NexusSensei <tudela.mathieu@gmail.com>
2025-03-14 16:38:44 +0100

    mise à jour du client sepolia

--- frontend/voting/app/CutsomRainbowKitProvider.js
+++ frontend/voting/app/CutsomRainbowKitProvider.js
@@ -6,20 +6,17 @@ import {
   lightTheme
 } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import {
-  sepolia,
-  hardhat
-} from 'wagmi/chains';
+//import { hardhat } from 'wagmi/chains';
 import {
   QueryClientProvider,
   QueryClient,
 } from "@tanstack/react-query";
-
+import { sepolia } from '@/utils/sepolia';
 
 const config = getDefaultConfig({
   appName: 'voting',
   projectId: 'e02d1c9b3c5856ff339e19638eed0a34',
-  chains: [sepolia, hardhat],
+  chains: [sepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
